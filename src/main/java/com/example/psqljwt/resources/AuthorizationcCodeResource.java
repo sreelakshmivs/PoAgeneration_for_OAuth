ccf7b96b79de1233186335c68059d8809296480d
sreelakshmivs <vssreelakshmi24@gmail.com>
2021-09-10 19:08:28 +0200

    Commit after readings taken

---
+++ src/main/java/com/example/psqljwt/resources/AuthorizationcCodeResource.java
@@ -0,0 +1,19 @@
+package com.example.psqljwt.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api/resourceserver")
+public class AuthorizationcCodeResource {
+    @GetMapping("")
+    public String getAuthorizationCode(HttpServletRequest request) {
+        //int clientCode = (Integer) request.getAttribute("userId");
+        //List<Category> categories = categoryService.fetchAllCategories(userId);
+        //return new ResponseEntity<>(categories, HttpStatus.OK);
+        return "Authorization code";
+    }
+}
