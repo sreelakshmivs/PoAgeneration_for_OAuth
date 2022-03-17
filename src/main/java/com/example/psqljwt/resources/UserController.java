064b7af2efc955201ea8970026ef1d5c97b3f493
sreelakshmivs <vssreelakshmi24@gmail.com>
2022-03-17 15:00:28 +0100

    Changed UserResource.java to UserController.java

--- src/main/java/com/example/psqljwt/resources/UserController.java
+++ src/main/java/com/example/psqljwt/resources/UserController.java
@@ -19,7 +19,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/subcontractor")
-public class UserResource {
+public class UserController {
 
     @Autowired
     UserService userService;
